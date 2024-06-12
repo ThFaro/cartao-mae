230c0bb4aaf3784138053eac8a97f48daaa6a460
Thiago Faro <thfaro.ads@gmail.com>
2024-06-12 11:40:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let currentPage = 0;
+
+document.getElementById('next').addEventListener('click', () => {
+  const pages = document.querySelector('.pages');
+  if (currentPage < pages.children.length - 1) {
+    currentPage++;
+    pages.style.transform = `rotateY(-${currentPage * 180}deg)`;
+  }
+});
+
+document.getElementById('prev').addEventListener('click', () => {
+  const pages = document.querySelector('.pages');
+  if (currentPage > 0) {
+    currentPage--;
+    pages.style.transform = `rotateY(-${currentPage * 180}deg)`;
+  }
+});
